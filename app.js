81048db24ce0597c5ec5619e832ea8e22142af5b
kripanshu mishra <mishrakripanshu303@gmail.com>
2022-04-11 03:18:21 +0530

    af

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+const Port = process.env.PORT || 5000;
+
+
+app.get('/',(req,res)=>{
+    res.json(require('./data.json'))
+})
+app.listen(Port,()=>{
+    console.log('server is running ')
+})
